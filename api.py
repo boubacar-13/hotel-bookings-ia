936132acc4258d48dd37c9c652dc3bb42d0f6c62
Boubacar <timera.boubacar@gmail.com>
2024-07-04 11:00:03 +0200

    edit def home()

--- api.py
+++ api.py
@@ -20,13 +20,12 @@ tags_metadata = [
 
 # Initialisation de l'application FastAPI
 app = FastAPI(
-    title="API Hotel Reservations",
+    title="API Réservations d'Hôtels",
     openapi_tags=tags_metadata,
     description="""   
-    Can you predict if customer is going to cancel the reservation ?
+    Pouvez-vous prédire si le client va annuler la réservation ?
     """,
 )
-
 
 # Définition de la classe pour les données d'entrée
 class Data(BaseModel):
@@ -52,7 +51,45 @@ class TrainingData(BaseModel):
 # Route de base
 @app.get("/", tags=["Text"])
 def home():
-    return {"message": "Page d'accueil API Hotel Reservation."}
+    return {
+        "message": "Bienvenue sur l'API Réservations d'Hôtels",
+        "description": "Cette API permet de prédire si un client va annuler sa réservation d'hôtel.",
+        "endpoints": {
+            "/training": "Endpoint POST pour l'entraînement du modèle.",
+            "/predict": "Endpoint POST pour faire des prédictions avec le modèle.",
+            "/docs": "Accédez à la documentation complète de l'API."
+        },
+        "examples": {
+            "training": {
+                "method": "POST",
+                "url": "/training",
+                "body": {
+                    "feature1": 10,
+                    "feature2": 20
+                }
+            },
+            "predict": {
+                "method": "POST",
+                "url": "/predict",
+                "body": {
+                    "no_of_adults": 2,
+                    "no_of_children": 1,
+                    "no_of_weekend_nights": 2,
+                    "no_of_week_nights": 3,
+                    "required_car_parking_space": 1,
+                    "lead_time": 100,
+                    "arrival_year": 2023,
+                    "arrival_month": 7,
+                    "arrival_date": 15,
+                    "repeated_guest": 0,
+                    "no_of_previous_cancellations": 0,
+                    "no_of_previous_bookings_not_canceled": 1,
+                    "avg_price_per_room": 150.0,
+                    "no_of_special_requests": 2
+                }
+            }
+        }
+    }
 
 # Endpoint POST pour l'entraînement du modèle
 @app.post("/training")
@@ -63,7 +100,7 @@ def training_endpoint(training_data: TrainingData):
         # Sauvegarder le modèle entraîné dans un fichier
         with open('modele_decision_tree.pkl', 'wb') as file:
             pickle.dump(trained_model, file)
-        return {"message": "Model trained successfully"}
+        return {"message": "Modèle entraîné avec succès"}
     except Exception as e:
         # Capturez les erreurs et renvoyez une réponse d'erreur appropriée
         raise HTTPException(status_code=500, detail=str(e))
